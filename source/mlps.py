85053fc9f6cc28af5af9760d7f8d132452e789dd
Ben Kabongo <kabongo.ben025@gmail.com>
2024-03-05 18:50:10 +0100

    Sampleable MLP: add functions

--- source/mlps.py
+++ source/mlps.py
@@ -10,43 +10,35 @@ class SampleableMLP(nn.Module):
     MLPs whose parameters can be sampled with MCMCs
 
     Parameters:
-    - input_size (int): model input size
-    - hidden_size (list(int)): model hiddens sizes list
-    - output_size (int): model output size
+    - sizes (list(int)): model sizes list
     - activations (list(nn.Module)): activation lists
         must contain an activation for each layer or None otherwise
     - temperature (float): temperature for log-target
 
     Attributes:
-    - multiclass: True for multilclass classification, False for binary classification
-
-    Methods:
-    - forward:
-    - num_parameters: returns the number of model parameters
-    - get_parameters: returns model parameters as 1D numpy array
-    - set_parameters: updates model parameters from a 1D array
-    - get_grad: returns model parameter gradients in the form of a 1D array
-    - set_grad: updates model parameter gradients from a 1D array
-    - compute_grad_log_target:
-    - 
+    - loss (nn.Module): CrossEntropyLoss for multiclass classification, BCELoss for binary classification
+    - prior (torch.distributions.Normal): prior distribution
     """
 
     def __init__(
-            self, 
-            input_size: int, 
-            hidden_sizes: List[int],
-            output_size: int,
+            self,
+            sizes: List[int],
             activations: List[nn.Module],
             temperature: float=1.,
         ):
         super(SampleableMLP, self).__init__()
-        sizes = [input_size] + hidden_sizes + [output_size]
+        self.sizes = sizes
         self.layers = nn.ModuleList([nn.Linear(sizes[i], sizes[i+1]) for i in range(len(sizes)-1)])
         self.activations = activations
         self.temperature = temperature
 
-        self.multiclass = (output_size > 1)
-        self.loss = nn.CrossEntropyLoss(reduction="sum") if self.multiclass else nn.BCELoss(reduction="sum")
+        is_multiclass = (self.sizes[-1] > 1)
+        self.loss = nn.CrossEntropyLoss(reduction="sum") if is_multiclass else nn.BCELoss(reduction="sum")
+
+        self.prior = torch.distributions.Normal(
+            loc=torch.zeros(self.num_parameters()),
+            scale=torch.ones(self.num_parameters())
+        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         for layer, activation in zip(self.layers, self.activations):
@@ -54,6 +46,38 @@ class SampleableMLP(nn.Module):
             if activation is not None:
                 x = activation(x)
         return x
+
+    def get_prior(self) -> torch.distributions.Normal:
+        """
+        Returns the prior distribution
+
+        Returns:
+        - prior (torch.distributions.Normal): prior distribution
+        """
+        return self.prior
+
+    def set_prior(
+            self, 
+            mu: Union[np.ndarray, torch.Tensor],
+            sigma: Union[np.ndarray, torch.Tensor]
+        ):
+        """
+        Set prior distribution
+
+        Parameters:
+        - mu (np.ndarray or torch.Tensor): mean of the distribution
+        - sigma (np.ndarray or torch.Tensor): standard deviation of the distribution
+        """
+        num_parameters = self.num_parameters()
+        assert len(mu) == num_parameters
+        assert len(sigma) == num_parameters
+
+        if isinstance(mu, np.ndarray):
+            mu = torch.from_numpy(mu)
+        if isinstance(sigma, np.ndarray):
+            sigma = torch.from_numpy(sigma)
+        
+        self.prior = torch.distributions.Normal(loc=mu, scale=sigma)
 
     def num_parameters(self) -> int:
         """
@@ -141,6 +165,31 @@ class SampleableMLP(nn.Module):
         """
         return torch.exp(self.compute_log_likelihood(x, y))
 
+    def compute_log_prior(self) -> torch.Tensor:
+        """
+        Calculation of the log prior
+
+        Returns:
+        - log prior (torch.Tensor): log prior
+        """
+        parameters = torch.from_numpy(self.get_parameters())
+        return self.temperature * torch.sum(self.prior.log_prob(parameters))
+
+    def compute_log_target(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        """
+        Calculation of the log target: log-likelihood + log prior
+
+        Parameters:
+        - x (torch.Tensor): input
+        - y (torch.Tensor): output
+
+        Returns:
+        - log target (torch.Tensor): log target
+        """
+        log_likelihood = self.compute_log_likelihood(x, y)
+        log_prior = self.compute_log_prior()
+        return log_likelihood + log_prior
+
     def compute_grad_log_target(self, log_target: torch.Tensor) -> np.ndarray:
         """
         Calculation of the gradient of model parameters based on log-likelihood 
@@ -154,16 +203,12 @@ class SampleableMLP(nn.Module):
         grad_log_target = autograd.grad(log_target, self.parameters(), create_graph=True)
         return np.concatenate([grad.cpu().detach().numpy().flatten() for grad in grad_log_target])
          
-    
-
 
 if __name__ == "__main__":
-    input_size = 2
-    hidden_sizes = [3, 2]
-    output_size = 2
+    sizes = [2, 1, 2, 3]
     activations = 3 * [nn.ReLU()] + [None]
 
-    mlp = SampleableMLP(input_size, hidden_sizes, output_size, activations)
+    mlp = SampleableMLP(sizes, activations)
     print("Number of parameters :", mlp.num_parameters())
 
     sampled_params = mlp.get_parameters()
